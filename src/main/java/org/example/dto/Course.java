351d203c22016248083a3b692db85ed7d597d63f
Ramanan1711 <ajayramanan17@gmail.com>
2024-09-03 15:46:15 +0530

    rds database commit in all class

--- src/main/java/org/example/dto/Course.java
+++ src/main/java/org/example/dto/Course.java
@@ -1,5 +1,10 @@
 package org.example.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +12,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "courses")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private double price;
